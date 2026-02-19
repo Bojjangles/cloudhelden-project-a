7a633b279ffd001661d85f3014c7b1a60dc7b83a
Your Name <bojan@Mac.fritz.box>
2026-02-19 12:12:51 +0100

    Day 3: VPC setup and EKS code (Clean backup)

---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+# terraform/main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Locked to v5 to avoid all bugs!
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1" # Changed from eu-central-1 to Stockholm!
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "project-a-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  map_public_ip_on_launch = true
+  enable_nat_gateway      = false
+  single_nat_gateway      = false
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+/* module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "project-a-cluster"
+  cluster_version = "1.28"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.public_subnets 
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    standard_nodes = {
+      min_size     = 0
+      max_size     = 4
+      desired_size = 2
+
+      instance_types = ["t3.medium"] 
+    }
+  }
+} 
+*/
